b6045f1f0f5adff33557020a9a295aa7d9399fd5
yzhou <yzhou@elijah.com.cn>
2015-12-16 10:35:59 +0800

    model config

---
+++ app/Discussion.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discussion extends Model
+{
+    protected $fillable = ['title','body','user_id','last_user_id'];
+}
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,8 +14,20 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
+        'avatar' => $faker->imageUrl(256,256),
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Discussion::class, function (Faker\Generator $faker) {
+    $user_ids = \App\User::lists('id')->toArray();
+
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'user_id' => $faker->randomElement($user_ids),
+        'last_user_id' =>$faker->randomElement($user_ids),
     ];
 });
