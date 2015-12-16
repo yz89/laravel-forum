86bd931cf68e91f54ae9f4360365cd6174bd86d0
yzhou <yzhou@elijah.com.cn>
2015-12-16 11:27:38 +0800

    belogsTo

--- app/Discussion.php
+++ app/Discussion.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Discussion extends Model
 {
     protected $fillable = ['title','body','user_id','last_user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
