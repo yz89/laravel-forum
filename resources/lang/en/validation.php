31e3f59495a3186e935e02f85f92ca57caaed029
yzhou <yzhou@elijah.com.cn>
2016-01-04 20:20:35 +0800

    validation

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -88,8 +88,8 @@ return [
     */
 
     'custom' => [
-        'attribute-name' => [
-            'rule-name' => 'custom-message',
+        'name' => [
+            'required' => '用户名不能为空',
         ],
     ],
 
